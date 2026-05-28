cc51f816bc94fa33b39d4d281c321dba3b4f4a90
vitaflowpy-ux <vitaflow.py@gmail.com>
2026-05-28 17:53:20 -0300

    Update infinitepay.js

--- netlify/functions/infinitepay.js
+++ netlify/functions/infinitepay.js
@@ -2,15 +2,12 @@ exports.handler = async function(event) {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' };
   }
-
-  const response = await fetch('https://api.infinitepay.io/invoices/public/checkout/links', {
+  const response = await fetch('https://api.checkout.infinitepay.io/links', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: event.body
   });
-
   const data = await response.text();
-
   return {
     statusCode: 200,
     headers: {
